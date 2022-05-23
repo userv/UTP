d233c00a220faaa27a269b15d06a4b0cd2fb62be
Rosen Angelov <rosen.t.angelov@gmail.com>
2022-05-23 12:00:31 +0300

    Remove check if you already in room.

--- www-data/static/js/server.js
+++ www-data/static/js/server.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const user_id = document.querySelector('#get-user_id').innerHTML;
 
     let room_id = '1';
-    let room_name;
+    let room_name = 'general';
 
     // Connect to websocket
     const socket = new WebSocket('ws://' + location.host + '/api');
@@ -109,85 +109,84 @@ document.addEventListener('DOMContentLoaded', () => {
         p.onclick = () => {
             let newRoom_name = p.innerHTML
             let newRoom_id = p.id
-            // Check if user already in the room
-            // leaveRoom(room_id);
-            // joinRoom(newRoom_id);
-            // room_id = newRoom_id;
-            // room_name = newRoom_name;
-            if (newRoom_id === room_id) {
-                let msg = `You are already in ${room_name} room.`;
-                printSysMsg(msg);
-            } else {
-                leaveRoom(room_id);
-                joinRoom(newRoom_id);
-                room_id = newRoom_id;
-                room_name = newRoom_name;
 
-            }
-        }
-    });
-
-        // Logout from chat
-        document.querySelector("#logout-btn").onclick = () => {
             leaveRoom(room_id);
-        };
-
-        // Trigger 'leave' event if user was previously on a room
-        function leaveRoom(room_id) {
-            let message = {
-                message: 'leave',
-                user_id: user_id,
-                username: username,
-                text: 'has left the room',
-                room_id: room_id,
-            }
-            // Join room
-            socket.send(JSON.stringify(message));
-
-            document.querySelectorAll('.select-room').forEach(p => {
-                p.style.color = "black";
-            });
-        }
-
-        // Trigger 'join' event
-        function joinRoom(room_id) {
-
-            let message = {
-                message: 'join',
-                user_id: user_id,
-                username: username,
-                text: 'has joined to room',
-                room_id: room_id,
-            }
-            // Join room
-            socket.send(JSON.stringify(message));
-
-            // Highlight selected room
-            document.querySelector('#' + CSS.escape(room_id)).style.color = "#ffc107";
-            document.querySelector('#' + CSS.escape(room_id)).style.backgroundColor = "white";
-
-            // Clear message area
-            document.querySelector('#display-message-section').innerHTML = '';
-
-            // Autofocus on text box
-            document.querySelector("#user_message").focus();
-        }
-
-        // Scroll chat window down
-        function scrollDownChatWindow() {
-            const chatWindow = document.querySelector("#display-message-section");
-            chatWindow.scrollTop = chatWindow.scrollHeight;
-        }
-
-        // Print system messages
-        function printSysMsg(msg) {
-            const p = document.createElement('p');
-            p.setAttribute("class", "system-msg");
-            p.innerHTML = msg;
-            document.querySelector('#display-message-section').append(p);
-            scrollDownChatWindow()
-
-            // Autofocus on text box
-            document.querySelector("#user_message").focus();
+            joinRoom(newRoom_id);
+            room_id = newRoom_id;
+            room_name = newRoom_name;
+            // if (newRoom_id === room_id) {
+            //     let msg = `You are already in ${room_name} room.`;
+            //     printSysMsg(msg);
+            // } else {
+            //     leaveRoom(room_id);
+            //     joinRoom(newRoom_id);
+            //     room_id = newRoom_id;
+            //     room_name = newRoom_name;
+            // }
         }
     });
+
+    // Logout from chat
+    document.querySelector("#logout-btn").onclick = () => {
+        leaveRoom(room_id);
+    };
+
+    // Trigger 'leave' event if user was previously on a room
+    function leaveRoom(room_id) {
+        let message = {
+            message: 'leave',
+            user_id: user_id,
+            username: username,
+            text: 'has left the room',
+            room_id: room_id,
+        }
+        // Join room
+        socket.send(JSON.stringify(message));
+
+        document.querySelectorAll('.select-room').forEach(p => {
+            p.style.color = "black";
+        });
+    }
+
+    // Trigger 'join' event
+    function joinRoom(room_id) {
+
+        let message = {
+            message: 'join',
+            user_id: user_id,
+            username: username,
+            text: 'has joined to room',
+            room_id: room_id,
+        }
+        // Join room
+        socket.send(JSON.stringify(message));
+
+        // Highlight selected room
+        document.querySelector('#' + CSS.escape(room_id)).style.color = "#ffc107";
+        document.querySelector('#' + CSS.escape(room_id)).style.backgroundColor = "white";
+
+        // Clear message area
+        document.querySelector('#display-message-section').innerHTML = '';
+
+        // Autofocus on text box
+        document.querySelector("#user_message").focus();
+    }
+
+    // Scroll chat window down
+    function scrollDownChatWindow() {
+        const chatWindow = document.querySelector("#display-message-section");
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+    }
+
+    // Print system messages
+    function printSysMsg(msg) {
+        const p = document.createElement('p');
+        p.setAttribute("class", "system-msg");
+        p.innerHTML = msg;
+        document.querySelector('#display-message-section').append(p);
+        scrollDownChatWindow()
+
+        // Autofocus on text box
+        document.querySelector("#user_message").focus();
+    }
+});
